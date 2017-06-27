b916fe586417e1e0cf481e306d9327034cbc71d0
Pawan Sasanka Ammanamanchi <Shashi456@users.noreply.github.com>
2017-06-27 17:12:20 +0530

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -13,6 +13,12 @@ for i in range(0,n):
             l[j-1],l[j]=l[j],l[j-1]
 print(l)
 print("count :",count)
-        
-        
+for i in range(0,n):
+    for j in range(1,n):
+        counti=counti+1
+        if(l1[j-1]>l1[j]):
+            l1[j-1],l1[j]=l1[j],l1[j-1]
+    if all(l1[i] <= l1[i+1] for i in range(len(l1)-1))== True:
+        break;
+print("counti :",counti)
     
